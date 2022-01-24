130b7f27520d159dd597d2985011d3ee8a223a98
Andrei Markov <84619111+Mark-Andre17@users.noreply.github.com>
2022-01-25 02:48:22 +0300

    Update settings.py

--- simple_crud/simple_crud/settings.py
+++ simple_crud/simple_crud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j$1(ig@@#+2-ismgv-!xayzd^g*9ryzf_h(pfk6%p18-5$yrl^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
